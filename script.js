409443bbf05e58e38cb3ac10c9b3d2bf23f9723e
w3project <adityabatgeri@gmail.com>
2023-01-03 16:57:56 +0530

    menu button

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-var obj = document.getElementById("off");
-obj.onclick = function()
+var obj1 = document.getElementById("off");
+obj1.onclick = function()
 {
-    obj.classList.toggle("on");
+    obj1.classList.toggle("on");
+}
+
+var obj2 = document.getElementById("menu");
+var obj3 = document.getElementById("sidebar");
+obj2.onclick = function()
+{
+    obj2.classList.toggle("menu1");
+    obj3.classList.toggle("sidebar1");
 }
