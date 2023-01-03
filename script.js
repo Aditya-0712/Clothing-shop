033123a61d0556c9ced0da4e78065f0d9d6dae94
w3project <adityabatgeri@gmail.com>
2023-01-03 17:27:39 +0530

    Improvisation

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var obj1 = document.getElementById("off");
 var obj4 = document.getElementById("lightbody");
+var obj5 = document.getElementById("sidecont");
 obj1.onclick = function()
 {
     obj1.classList.toggle("on");
     obj4.classList.toggle("darkbody");
+    obj5.classList.toggle("darksidecont");
+    obj3.classList.toggle("darksidebar");
 }
 
 var obj2 = document.getElementById("menu");
