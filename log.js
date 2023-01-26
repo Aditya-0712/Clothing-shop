a392cff4c3e68a5213500fea6f169212ffcef742
w3project <adityabatgeri@gmail.com>
2023-01-26 17:41:15 +0530

    validation

--- log.js
+++ log.js
@@ -5,3 +5,14 @@ obj1.onclick = function()
     obj1.classList.toggle("on");
     obj2.classList.toggle("darkbody");
 }
+
+var pw = document.getElementById("pass").value;
+
+function passlen()
+{
+    if (pw.length<8)
+    {
+        alert ("Password must be atleast 8 characters");
+        return false;
+    }
+}
