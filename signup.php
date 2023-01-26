8ae52ba8e4667555af1d2508cdf51344809c5cba
w3project <adityabatgeri@gmail.com>
2023-01-26 12:03:16 +0530

    signup complete

---
+++ signup.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Shop | Home</title>
+        <meta name="viewport" content="width=device-width , initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="font1.css">
+    </head>
+    <body class="lightbody" id="lightbody">
+
+        <!-- HTML For Desktop Version -->
+        <div class="topnav" id="topnav">
+            <p class="toptext">CLOTHEX</p>
+            <a class="dropdown" id="dropdown">
+                <p class="newoptp">Categories</p>
+                <img src="arrowdown.svg" class="down">
+            </a>
+            <a href="#new" class="newopt">
+                <p class="newoptp">Discounts</p>
+            </a>
+            <a href="#new" class="newopt">
+                <p class="newoptp">Your Cart</p>
+                <img src="cart.svg" class="newcart">
+            </a>
+            <a href="login.html" class="newopt">
+                <p class="newoptp">Account</p>
+                <img src="account.svg" class="acc">
+            </a>
+            <div class="newopt">
+                <p class="newoptp">Dark Mode</p>
+                <div class="off" id="off1">
+                    <div class="div1"></div>
+                </div>
+            </div>
+        </div>
+        <div class="alt">
+            <div class="alt1"></div>
+            <div class="alt2" id="alt2">
+                <p class="altopt1">MEN</p>
+                <p class="rem">WOMEN</p>
+                <p class="rem">KIDS</p>
+                <p class="rem">SEASONAL</p>
+            </div>
+            <div class="alt3"></div>
+            <div class="alt4"></div>
+            <div class="alt5"></div>
+            <div class="alt6"></div>
+        </div>
+
+        <!-- HTML For Mobile Version -->
+        <div class="sidebar" id="sidebar">
+            <p class="sidecont" id="sidecont">DARK<br>THEME</p>
+            <div class="off" id="off">
+                <div class="div1"></div>
+            </div>
+            <p class="option" id="option">MEN</p>
+            <p class="option" id="option1">WOMEN</p>
+            <p class="option" id="option2">KIDS</p>
+            <p class="option" id="option3">SEASONAL</p>
+            <p class="option" id="option4">DISCOUNTS</p>
+        </div>
+        <div class="menu" id="menu">
+            <div class="span1"></div>
+            <div class="span2"></div>
+            <div class="span3"></div>
+        </div>
+        <p class="title" id="title">CLOTHEX</p>
+        <div class="invis">
+            <a href="login.html"><img class="mobcart" src="cart.svg"></a>
+        </div>
+        <!-- <div class="invis2">
+            <img src="account.svg" class="mobacc">
+        </div> -->
+
+        </div>
+        <p class="exc" id="title1">!! EXCLUSIVES !!</p>
+
+        <div class="deskflex" id="dimg1">
+            <img src="modalt.png" class="deskimg">
+            <div class="flextext">
+                <p class="ftitle">DENIM  JACKET</p>
+                <p class="prodesc">Ocean Blue Denim Jacket with White T-Shirt<br>Available in all sizes</p>
+                <img src="stars.png" class="prostars">
+                <p class="proprice"><cite>USD 340.00</cite></p>
+                <div class="procart" id="deskcart1">
+                    <p class="add">ADD TO CART</p>
+                    <img src="cart.svg" class="prosvg">
+                </div>
+            </div>
+        </div>
+        <div class="deskflex" id="dimg2">
+            <div class="flextext">
+                <p class="ftitle">PLAIN T-SHIRTS</p>
+                <p class="prodesc">Plain T-Shirts of any color<br>Available in all sizes</p>
+                <img src="stars.png" class="prostars">
+                <p class="proprice"><cite>USD 200.00</cite></p>
+                <div class="procart" id="deskcart2">
+                    <p class="add">ADD TO CART</p>
+                    <img src="cart.svg" class="prosvg">
+                </div>
+            </div>
+            <img src="mod2.png" class="deskimg1">
+        </div>
+        <div class="deskflex" id="dimg3">
+            <img src="mod3.png" class="deskimg">
+            <div class="flextext">
+                <p class="ftitle">VINTAGE  SNEAKERS</p>
+                <p class="prodesc">Navy Blue Vintage College Sneakers<br>Available in all sizes</p>
+                <img src="stars.png" class="prostars">
+                <p class="proprice"><cite>USD 210.00</cite></p>
+                <div class="procart" id="deskcart3">
+                    <p class="add">ADD TO CART</p>
+                    <img src="cart.svg" class="prosvg">
+                </div>
+            </div>
+        </div>
+        <div class="deskflex" id="dimg4">
+            <div class="flextext">
+                <p class="ftitle">WINTER  WEARS</p>
+                <p class="prodesc">Woolen Cozy Full Sweaters<br>Available in all colors</p>
+                <img src="stars.png" class="prostars">
+                <p class="proprice"><cite>USD 170.00</cite></p>
+                <div class="procart" id="deskcart4">
+                    <p class="add">ADD TO CART</p>
+                    <img src="cart.svg" class="prosvg">
+                </div>
+            </div>
+            <img src="mod4.png" class="deskimg1">
+        </div>
+
+        <img src="modalt.png" class="modf" id="mod1">
+        <p class="hed" id="hed1">DENIM  JACKET</p>
+        <p class="desc">Ocean Blue Denim Jacket with White T-Shirt<br>Available in all sizes</p>
+        <img src="stars.png" class="stars">
+        <p class="price"><cite>USD 340.00</cite></p>
+        <div class="cart" id="cart">
+            <p class="add">ADD TO CART</p>
+            <img src="cart.svg" class="svg">
+        </div>
+        <img src="mod2.png" class="mod1" id="mod2">
+        <p class="hed" id="hed2">PLAIN T-SHIRTS</p>
+        <p class="desc">Plain T-Shirts of any color<br>Available in all sizes</p>
+        <img src="stars.png" class="stars">
+        <p class="price"><cite>USD 200.00</cite></p>
+        <div class="cart" id="cart2">
+            <p class="add">ADD TO CART</p>
+            <img src="cart.svg" class="svg">
+        </div>
+        <img src="mod3.png" class="mod1" id="mod3">
+        <p class="hed" id="hed3">VINTAGE  SNEAKERS</p>
+        <p class="desc">Navy Blue Vintage College Sneakers<br>Available in all sizes</p>
+        <img src="stars.png" class="stars">
+        <p class="price"><cite>USD 210.00</cite></p>
+        <div class="cart" id="cart3">
+            <p class="add">ADD TO CART</p>
+            <img src="cart.svg" class="svg">
+        </div>
+        <img src="mod4.png" class="mod1" id="mod4">
+        <p class="hed" id="hed4">WINTER  WEARS</p>
+        <p class="desc">Woolen Cozy Full Sweaters<br>Available in all colors</p>
+        <img src="stars.png" class="stars">
+        <p class="price"><cite>USD 170.00</cite></p>
+        <div class="cart" id="cart4">
+            <p class="add">ADD TO CART</p>
+            <img src="cart.svg" class="svg">
+        </div>
+        <div class="cred">
+            <p class="credcont">Created By</p>
+            <p class="newtitle">ADITYA  BATGERI</p> 
+
+            <div class="hor">
+                <div class="prolinq">
+                    <p class="name">Instagram</p>
+                    <img class="insta" src="instagram.webp">
+                </div>
+                <div class="prolinq">
+                    <p class="name">Twitter</p>
+                    <img class="insta" src="twitter.png">
+                </div>
+                <div class="prolinq">
+                    <p class="name">GitHub</p>
+                    <img class="insta" src="github.png">
+                </div>
+                <div class="prolinq">
+                    <p class="name">Discord</p>
+                    <img class="insta" src="discord.png">
+                </div>
+            </div>
+
+            <div class="linq">
+                <p class="name">Instagram</p>
+                <img class="insta" src="instagram.webp">
+            </div>
+            <div class="linq">
+                <p class="name">Twitter</p>
+                <img class="insta" src="twitter.png">
+            </div>
+            <div class="linq">
+                <p class="name">GitHub</p>
+                <img class="insta" src="github.png">
+            </div>
+            <div class="linq">
+                <p class="name">Discord</p>
+                <img class="insta" src="discord.png">
+            </div>
+            <hr class="hr">
+            <p class="final">All rights reserved</p>
+            <p class="final">© 2023 Aditya Batgeri</p>
+        </div>
+        <script src="script.js"></script>
+    </body>
+</html>
+
+<?php
+
+$email = $_POST["email"];
+$user = $_POST["user"];
+$pass = $_POST["pass"];
+
+$conn = new mysqli("localhost","root","","clothex.com");
+if($conn->connect_error)
+{
+    die("Connection Failed: " . $conn->connect_error);
+}
+else 
+{
+    $stmt = $conn->prepare("INSERT INTO clothex_signup(Email,Username,Password) VALUES(?,?,?)");
+    $stmt->bind_param("sss", $email , $user , $pass);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+}
+
+?>
